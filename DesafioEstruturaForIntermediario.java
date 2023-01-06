04fc355a1b2a364b56afb224eefa09527409369a
sioliv <silvinha.olivert@gmail.com>
2023-01-06 20:11:05 -0300

    Desafio estrutura for intermediário

--- DesafioEstruturaForIntermediario.java
+++ DesafioEstruturaForIntermediario.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 public class DesafioEstruturaForIntermediario {
 
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+
+        int quantidadeDivisiveis = 0;
+
+        System.out.println("Digite um número: ");
+        numero = entrada.nextInt();
+
+        for(int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+
+                quantidadeDivisiveis++;
+            }
+
+        }
+
+        if (quantidadeDivisiveis == 2) {
+            System.out.println("Numero Primo!!");
+        } else  {
+            System.out.println("Numero não é primo");
+        }
+
+        entrada.close();
+
+    }
 
 }
