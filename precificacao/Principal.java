72b82d637cb007588bb6f08b8c06b89f76794f62
sioliv <silvinha.olivert@gmail.com>
2023-01-18 09:21:22 -0300

    Metodos que alteram o valor do parametro do tipo primitivo

---
+++ precificacao/Principal.java
@@ -0,0 +1,18 @@
+package precificacao;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        double precoCompraFornecedor = 140;
+
+        ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
+
+        servicoDePrecificacao.calcularPrecoVenda(precoCompraFornecedor);
+
+        System.out.printf("Preço: %.2f%n", precoCompraFornecedor);
+
+    }
+
+
+}
---
+++ precificacao/ServicoDePrecificacao.java
@@ -0,0 +1,13 @@
+package precificacao;
+
+public class ServicoDePrecificacao {
+
+    void calcularPrecoVenda(double precoCusto){
+        //passo o valor dessa variavel. altera somente a variável local
+        //NÃO É UMA BOA PRATICA MODIFICAR OS VALORES DOS PARÂMETROS.
+        precoCusto = precoCusto * 1.20;
+
+        //faz várias coisas
+     }
+
+}
