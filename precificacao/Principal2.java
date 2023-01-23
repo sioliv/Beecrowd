b09c8c5c592fe002eda35961900b399581c39ae6
sioliv <silvinha.olivert@gmail.com>
2023-01-23 08:55:21 -0300

    metodo de instancia alterando variavel estatica

--- precificacao/Principal2.java
+++ precificacao/Principal2.java
@@ -7,10 +7,8 @@ public class Principal2 {
         Produto produto1 = new Produto();
         Produto produto2 = new Produto();
 
-        Produto.custoEmbalagem = 20;
-
-//        produto1.custoEmbalagem = 10;
-//        produto2.custoEmbalagem = 15;
+        produto1.alterarCustoEmbalagem(12);
+        produto2.alterarCustoEmbalagem(20);
 
         produto1.imprimirCustoEmbalagem();
         produto2.imprimirCustoEmbalagem();
