117296d489412c18773c34fcb1fe115f332aa627
sioliv <silvinha.olivert@gmail.com>
2023-01-29 17:02:30 -0300

    wrapper box

---
+++ WrappersBoxing/Principal1.java
@@ -0,0 +1,20 @@
+package WrappersBoxing;
+
+import javax.lang.model.element.NestingKind;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        //Tipos primitivos
+
+        int diasParaEntrega;
+        long codigoEntrega;
+        float valorFrete;
+        double valorTotal;
+        char tipoCliente;
+        boolean compraPaga;
+
+        //String é uma classe
+        String nomeCliente;
+    }
+}
